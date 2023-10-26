6211f3f697dafdbdc8087acf73cb6858e804b467
Werneck <werneckcolins@outlook.com>
2023-10-25 21:21:33 -0300

    Header funcionando

--- js/tatoo.js
+++ js/tatoo.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     // Selecionar o link de seta
     var arrowLink = document.querySelector(".fa-solid.fa-arrow-up");
@@ -9,27 +10,36 @@ document.addEventListener("DOMContentLoaded", function() {
     arrowLink.addEventListener("click", function(event) {
         event.preventDefault(); // Prevenir o comportamento padrão do link
         header.classList.add("hidden"); // Adicionar a classe "hidden" para ocultar o header
+
+        console.log("funcinou")
     });
+
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Selecionar o link da seta
-  var arrowLink = document.querySelector(".fa-solid.fa-arrow-up");
 
-  // Selecionar o elemento "header" pelo ID
-  var header = document.getElementById("header");
 
-  // Adicionar um ouvinte de evento de clique ao link
-  arrowLink.addEventListener("click", function(event) {
-      event.preventDefault(); // Prevenir o comportamento padrão do link
 
-      // Verificar se o "header" está visível
-      if (header.style.display === "block" || header.style.display === "") {
-          header.style.display = "none"; // Ocultar o header
-          arrowLink.classList.add("visible"); // Adicionar classe "visible" para mostrar a seta
-      } else {
-          header.style.display = "block"; // Mostrar o header
-          arrowLink.classList.remove("visible"); // Remover classe "visible" para ocultar a seta
-      }
-  });
-});
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+
+//   var arrowLink = document.querySelector(".fa-solid.fa-arrow-up");
+
+
+//   var header = document.getElementById("header");
+
+  
+//   arrowLink.addEventListener("click", function(event) {
+//       event.preventDefault(); 
+
+     
+//       if (header.style.display === "block" || header.style.display === "") {
+//           header.style.display = "none"; 
+//           arrowLink.classList.add("visible"); 
+//       } else {
+//           header.style.display = "block";
+//           arrowLink.classList.remove("visible"); 
+//       }
+//   });
+// });
