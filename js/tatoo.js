79d447aef261f5ee2a65d09f6db49236189ead8b
Werneck <werneckcolins@outlook.com>
2023-11-03 14:19:32 -0300

    Atualizado página inicial

--- js/tatoo.js
+++ js/tatoo.js
@@ -1,45 +1,15 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-    // Selecionar o link de seta
-    var arrowLink = document.querySelector(".fa-solid.fa-arrow-up");
-
-    // Selecionar o elemento "header" pelo ID
+    var arrowLink = document.querySelector("#seta");
     var header = document.getElementById("header");
+    var arrowIcon = arrowLink.querySelector("#seta");
 
-    // Adicionar um ouvinte de evento de clique ao link
     arrowLink.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevenir o comportamento padrão do link
-        header.classList.add("hidden"); // Adicionar a classe "hidden" para ocultar o header
+        event.preventDefault(); 
+        header.classList.toggle("hidden"); 
+        arrowIcon.classList.toggle("rotate");
 
-        console.log("funcinou")
+        console.log("funcionou")
     });
 
 });
 
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-
-//   var arrowLink = document.querySelector(".fa-solid.fa-arrow-up");
-
-
-//   var header = document.getElementById("header");
-
-  
-//   arrowLink.addEventListener("click", function(event) {
-//       event.preventDefault(); 
-
-     
-//       if (header.style.display === "block" || header.style.display === "") {
-//           header.style.display = "none"; 
-//           arrowLink.classList.add("visible"); 
-//       } else {
-//           header.style.display = "block";
-//           arrowLink.classList.remove("visible"); 
-//       }
-//   });
-// });
