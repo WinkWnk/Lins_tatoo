87b088d4a8f669152cbc771866ca1450517c10d6
Werneck <werneckcolins@outlook.com>
2023-11-08 20:38:24 -0300

    Seta funcionando

--- js/tatoo.js
+++ js/tatoo.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     var arrowLink = document.querySelector("#seta");
     var header = document.getElementById("header");
-    var arrowIcon = arrowLink.querySelector("#seta");
 
     arrowLink.addEventListener("click", function(event) {
         event.preventDefault(); 
         header.classList.toggle("hidden"); 
-        arrowIcon.classList.toggle("rotate");
-
-        console.log("funcionou")
-    });
-
+        arrowLink.classList.toggle("rotate");
+    });   
 });
 
